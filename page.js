202d8f8a4c33d88abfb8f8eaf159dede51bcef6c
Sanjay Gonsalves <snjgonsalves@gmail.com>
2024-07-31 22:29:22 -0700

    completed automation test cases

--- page.js
+++ page.js
@@ -1,18 +1,41 @@
 module.exports = {
+
     // Inputs
     fromField: '#from',
     toField: '#to',
     phoneNumberField: '#phone',
     codeField: '#code',
+    cardNumberInput: '.card-number-input #number',
+    cardCode: '.card-code-input #code',
+    messageInput: '#comment',
+
     // Buttons
     callATaxiButton: 'button=Call a taxi',
     phoneNumberButton: '//div[starts-with(text(), "Phone number")]',
     nextButton: 'button=Next',
     confirmButton: 'button=Confirm',
+    paymentButton: '.pp-button',
+    supportiveButton: 'div=Supportive',
+    addCardButton: '.pp-row.disabled',
+    linkButton: '//button[contains(text(), "Link")]',
+    closeCardModalButton: '.payment-picker .close-button',
+    iceCreamPlusButton: '.counter-plus',
+    orderButton: '.smart-button',
+
+    // Checkboxes
+    blanketsSwitch: '.r-sw',
+
+    // Counters
+    iceCreamCounter: '.counter-value',
+
     // Modals
     phoneNumberModal: '.modal',
+    paymentModal: '.payment-picker .modal .section',
+    orderModal: '.order-body',
+   
+
     // Functions
-    fillAddresses: async function(from, to) {
+    fillAddresses: async function (from, to) {
         const fromField = await $(this.fromField);
         await fromField.setValue(from);
         const toField = await $(this.toField);
@@ -21,7 +44,7 @@ module.exports = {
         await callATaxiButton.waitForDisplayed();
         await callATaxiButton.click();
     },
-    fillPhoneNumber: async function(phoneNumber) {
+    fillPhoneNumber: async function (phoneNumber) {
         const phoneNumberButton = await $(this.phoneNumberButton);
         await phoneNumberButton.waitForDisplayed();
         await phoneNumberButton.click();
@@ -31,7 +54,7 @@ module.exports = {
         await phoneNumberField.waitForDisplayed();
         await phoneNumberField.setValue(phoneNumber);
     },
-    submitPhoneNumber: async function(phoneNumber) {
+    submitPhoneNumber: async function (phoneNumber) {
         await this.fillPhoneNumber(phoneNumber);
         // we are starting interception of request from the moment of method call
         await browser.setupInterceptor();
@@ -48,4 +71,46 @@ module.exports = {
         await codeField.setValue(code)
         await $(this.confirmButton).click()
     },
+    clickSupportiveButton: async function () {
+        const supportiveButton = await $(this.supportiveButton);
+        await supportiveButton.click()
+    },
+    clickPaymentButton: async function () {
+        const paymentButton = await $(this.paymentButton);
+        await paymentButton.click();
+    },
+    fillCardDetails: async function (cardNumber, cvvNumber, linkAndClose = false) {
+        const addCardButton = await $(this.addCardButton);
+        await addCardButton.click();
+        await $(this.cardNumberInput).setValue(cardNumber);
+        await $(this.cardCode).setValue(cvvNumber);
+        await browser.keys("\ue004");
+        if (linkAndClose) {
+            await ($(this.linkButton)).click();
+            await $(this.closeCardModalButton).click(); // close payment window
+        }
+    },
+    writeMessage: async function (message = 'Sample Message') {
+        const messageInputBox = $(this.messageInput);
+        await messageInputBox.setValue(message);
+    },
+    clickBlanketSwitch: async function () {
+        const blanketsSwitch = await $(this.blanketsSwitch);
+        await blanketsSwitch.waitForDisplayed();
+        await blanketsSwitch.scrollIntoView();
+        await blanketsSwitch.click();
+    },
+    addTwoIceCreams: async function () {
+        const iceCreamPlusButton = await $(this.iceCreamPlusButton);
+        await iceCreamPlusButton.waitForDisplayed();
+        await iceCreamPlusButton.scrollIntoView();
+        await iceCreamPlusButton.click();
+        await iceCreamPlusButton.click();
+    },
+    clickOrderButton: async function () {
+        const orderButton = await $(this.orderButton);
+        await orderButton.waitForDisplayed();
+        await orderButton.click();
+    }
+
 };
--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -1,23 +1,67 @@
 const page = require('../../page');
-const helper = require('../../helper')
+const helper = require('../../helper');
 
 describe('Create an order', () => {
-    it('should open phone number modal', async () => {
-        await browser.url(`/`)
+  
+    it('should complete the process of ordering a taxi', async () => {
+        await browser.url(`/`);
+        
+        // Set the addresses
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumberButton = await $(page.phoneNumberButton);
-        await phoneNumberButton.waitForDisplayed();
-        await phoneNumberButton.click();
-        const pnoneNumberModal = await $(page.phoneNumberModal);
-        await expect(pnoneNumberModal).toBeExisting();
-    })
-
-    it('should save the phone', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumber = helper.getPhoneNumber("+1");
+        
+        // Select Supportive plan
+        await page.clickSupportiveButton();
+        
+        // Generate and fill in the phone number
+        const phoneNumber = helper.getPhoneNumber('+1');
         await page.submitPhoneNumber(phoneNumber);
         await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
-    })
-})
+        
+        // Add a credit card
+        await page.clickPaymentButton();
+        const cardNumber = helper.getCardNumber();
+        const cvvNumber = helper.getCVVNumber();
+        await page.fillCardDetails(cardNumber, cvvNumber, true);
+        
+        // Write a message for the driver
+        const message = 'Test';
+        await page.writeMessage(message);
+        await expect($(page.messageInput)).toHaveValue(message);
+        
+         // Order a blanket and handkerchiefs
+         await page.clickBlanketSwitch();
+         await browser.pause(500); // Small pause to ensure the switch state changes
+         
+        // Verify the blanket switch state by checking for the active class
+        const blanketsSwitch = await $(page.blanketsSwitch);
+        const classList = await blanketsSwitch.getAttribute('class');
+        console.log(`Blanket switch class list: ${classList}`);
+        await expect(classList).toContain('r-sw'); 
+        
+        // Order 2 ice creams
+        await page.addTwoIceCreams();
+        const iceCreamCounter = await $(page.iceCreamCounter);
+        await iceCreamCounter.waitForDisplayed();
+        const iceCreamCountText = await iceCreamCounter.getText();
+        console.log(`Ice cream counter value: ${iceCreamCountText}`);
+        await expect(iceCreamCountText).toBe('2');
+        
+        // Order the taxi
+        await page.clickOrderButton();
+        
+        // Scroll to ensure order modal is in view
+        console.log("Scrolling to order modal...");
+        const orderModal = await $(page.orderModal);
+        await orderModal.scrollIntoView();
 
+        // Wait for the order modal to appear
+        console.log("Waiting for the order modal to appear...");
+        await orderModal.waitForDisplayed({ timeout: 60000 });
+
+        // Verify the driver info in the order modal
+        console.log("Verifying driver info in the order modal...");
+        const driverInfo = await orderModal.$('//div[contains(text(), "The driver will arrive in")]'); // Use a specific text or element inside the modal that indicates driver info
+        await driverInfo.waitForDisplayed({ timeout: 60000 });
+        await expect(driverInfo).toBeDisplayed();
+    });
+});
